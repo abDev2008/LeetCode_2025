1a32e6cce7fd83d2c87c7591dbd7b0bc44ee215c
Abel Trfneh <134791272+abDev2008@users.noreply.github.com>
2025-03-06 18:44:34 -0600

    Time: 19 ms (22.94%), Space: 68.4 MB (93.51%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
@@ -1,25 +1,25 @@
 class ProductOfNumbers {
-    ArrayList<Integer> list = new ArrayList<>();
-    int prod = 1;
+    private List<Integer> prefix;
 
     public ProductOfNumbers() {
-        
+        prefix = new ArrayList<>();
+        prefix.add(1);
     }
-    
+
     public void add(int num) {
-        if(num == 0){
-            list = new ArrayList<>();
-            prod = 1;
-            return;
+        if (num == 0) {
+            prefix.clear();
+            prefix.add(1);
+        } else {
+            prefix.add(num * prefix.get(prefix.size() - 1));
         }
-        prod = prod * num;
-        list.add(prod);
     }
-    
+
     public int getProduct(int k) {
-        if(list.size() < k) return 0;
-        int ans = list.get(list.size() - 1);
-        if(list.size() == k) return ans;
-        return ans / list.get(list.size() - 1 - k);
+        int n = prefix.size();
+        if (k >= n) {
+            return 0;
+        }
+        return prefix.get(n - 1) / prefix.get(n - 1 - k);
     }
 }
